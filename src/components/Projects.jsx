db06ce8dd0c5a7a7a6cb0fc38e72f1e6c437cfc8
Safiyah <safiyahmasud@gmail.com>
2024-10-01 13:56:40 +0100

    home

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,13 +8,13 @@ function Projects() {
 		<section id="projects">
 			<div className="lg:pl-40 lg:w-4/5 text-xl">
 				<h1 className="text-3xl sm:text-xl pb-20 sm:pb-10">Case Studies</h1>
-				<div className="bg-p1 rounded-2xl flex flex-col gap-10 px-16 py-10 md:px-10 sm:px-3">
-					<div className="flex flex-col sm:self-center gap-10 w-max">
+				<div className="bg-p1 shadow-light-3xl rounded-2xl flex flex-col gap-10 px-16 py-10 md:px-10 sm:px-3">
+					<div className="flex flex-col self-center gap-10 w-max">
 						<div className="flex justify-center items-center">
 							<img
 								src={Phone}
 								alt=""
-								className="h-64 object-cover rounded-t-2xl sm:hidden md:hidden"
+								className="h-64 relative object-cover rounded-t-2xl hidden sm:hidden md:hidden"
 							/>
 							<img
 								src={Nanny}
