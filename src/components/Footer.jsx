00ebc072f1f37529228a0d6d2e4bddff57b46d98
Safiyah <safiyahmasud@gmail.com>
2025-07-06 00:54:55 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -50,7 +50,7 @@ function Footer() {
 								CV
 							</Link>
 							<Link
-								to={"www.linkedin.com/in/safiyah-amedu-841229370"}
+								to={"https://www.linkedin.com/in/safiyah-amedu-841229370/
 								target="_blank"
 								rel="noopener noreferrer"
 							>
